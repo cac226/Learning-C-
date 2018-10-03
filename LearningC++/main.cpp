b6a443e54e53500440f96269d4543428e30fbe22
cac226 <cac226@case.edu>
2018-10-03 13:42:22 -0400

    Initial Commit

---
+++ LearningC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningC++
+//
+//  Created by Caitlin Campbell on 10/3/18.
+//  Copyright © 2018 Caitlin Campbell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
